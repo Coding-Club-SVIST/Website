4d6ca3da67db4ee75486e45607ab2345f8926d68
ServerDeveloper9447 <86161882+ServerDeveloper9447@users.noreply.github.com>
2026-02-04 21:48:11 +0530

    Updated title and meta tags

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -4,6 +4,18 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <title>Coderride | SVIST</title>
+        <meta name="description" content="A platform for coders to share their projects, skills and experiences." />
+        <meta name="keywords" content="coderride, projects, skills, experiences, coding, developers, svist" />
+        <meta name="author" content="SVIST" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#131C2E" />
+        <meta property="og:title" content="Coderride | SVIST" />
+        <meta property="og:description" content="A platform for coders to share their projects, skills and experiences." />
+        <meta property="og:url" content="https://coderride.foo.ng" />
+        <meta property="og:site_name" content="Coderride" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en_US" />
       </Head>
       <body>
         <Main />
